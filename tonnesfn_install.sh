35d53eef4d0ceac728387cdd306868a5fc0310f3
Tønnes Nygaard <tonnesfn@ifi.uio.no>
2018-05-22 15:44:37 +0200

    Added ROS option

--- tonnesfn_install.sh
+++ tonnesfn_install.sh
@@ -12,7 +12,8 @@ sudo apt-get install dialog
   options=(1 "Git++" off
            2 "VLC" off
            3 "Chrome" off
-           4 "Slack" off)
+           4 "Slack" off
+           5 "ROS" off)
   choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
   clear
   for choice in $choices
@@ -55,10 +56,27 @@ sudo apt-get install dialog
     sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
     apt-get update 
     apt-get install google-chrome-stable -y
+    echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
   ;;
 
   4) #Slack
     snap install slack --classic
+  ;;
+
+  5) #ROS
+    sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+    apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+    apt-get update
+    apt-get install ros-melodic-desktop-full -y
+    rosdep init
+    rosdep update
+    apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential -y
+
+    wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+    apt-get update
+    apt-get install python-catkin-tools -y
+    
+    sudo -H -u tonnesfn bash -c 'mkdir -p ~/catkin_ws/src && source /opt/ros/melodic/setup.bash  && cd ~/catkin_ws && catkin build' 
   ;;
 
   esac
