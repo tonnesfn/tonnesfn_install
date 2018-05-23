029ea90836215e5c08b26699fcab853d3d1ed281
Tønnes Nygaard <tonnesfn@ifi.uio.no>
2018-05-23 09:23:08 +0200

    Added DyRET repos and compressed menu

--- tonnesfn_install.sh
+++ tonnesfn_install.sh
@@ -9,19 +9,29 @@ else
 
 sudo apt-get install dialog
   cmd=(dialog --separate-output --checklist "Please Select Software you want to install:" 22 76 16)
-  options=(1 "Git++" off
-           2 "VLC" off
+  options=(1 "Various small programs" off
+           2 "Git++" off
            3 "Chrome" off
-           4 "Slack" off
-           5 "ROS" off)
+           4 "ROS" off
+           5 "GVIM" off
+           6 "Get DyRET code" off)
   choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
   clear
   for choice in $choices
   do
   case $choice in
 
-  1) #Install Git
-    echo "Installing Git++"
+  1) # Misc programs
+    echo "Installing VLC"
+    apt-get install vlc -y
+    echo "Installing Slack"
+    snap install slack --classic
+    echo "Installing Terminator"
+    apt-get install terminator -y
+  ;;
+
+  2) # Install Git++
+    echo "Installing Git"
 
     # Install git
     apt install git -y
@@ -36,21 +46,16 @@ sudo apt-get install dialog
 
     # Configure Git
     git config --global user.email "tonnes.nygaard@gmail.com"
-    git config --global user.name "tonnesfn"
+    git config --global user.name $SUDO_USER
 
     # Make keys and open
-    sudo -H -u tonnesfn bash -c 'mkdir $HOME/.ssh'
-    sudo -H -u tonnesfn bash -c 'ssh-keygen -t rsa -b 4096 -C "tonnes.nygaard@gmail.com" -f $HOME/.ssh/id_rsa'
-    sudo -H -u tonnesfn bash -c 'eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_rsa' 
+    sudo -H -u $SUDO_USER bash -c 'mkdir $HOME/.ssh'
+    sudo -H -u $SUDO_USER bash -c 'ssh-keygen -t rsa -b 4096 -C "tonnes.nygaard@gmail.com" -f $HOME/.ssh/id_rsa'
+    sudo -H -u $SUDO_USER bash -c 'eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_rsa' 
     gedit ~/.ssh/id_rsa.pub &
   ;;
 
-  2) #VLC
-    echo "Installing VLC"
-    apt-get install vlc -y
-  ;;
-
-  3) #Chrome
+  3) # Chrome
     echo "Installing Google Chrome"
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
     sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
@@ -59,11 +64,7 @@ sudo apt-get install dialog
     echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
   ;;
 
-  4) #Slack
-    snap install slack --classic
-  ;;
-
-  5) #ROS
+  4) # ROS
     sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
     apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
     apt-get update
@@ -76,7 +77,22 @@ sudo apt-get install dialog
     apt-get update
     apt-get install python-catkin-tools -y
     
-    sudo -H -u tonnesfn bash -c 'mkdir -p ~/catkin_ws/src && source /opt/ros/melodic/setup.bash  && cd ~/catkin_ws && catkin build' 
+    sudo -H -u $SUDO_USER bash -c 'mkdir -p ~/catkin_ws/src && source /opt/ros/melodic/setup.bash  && cd ~/catkin_ws && catkin build' 
+    echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+  ;;
+
+  5) # GVIM
+    apt-get install vim-gnome -y
+    #TODO: Add gvim settings
+  ;;
+
+  6) # Get DyRET git repos
+    sudo -H -u $SUDO_USER bash -c 'cd ~/catkin_ws/src && git clone git@github.uio.no:robin/dyret_common.git'
+    sudo -H -u $SUDO_USER bash -c 'cd ~/catkin_ws/src && git clone git@github.uio.no:robin/dyret_controller.git'
+    sudo -H -u $SUDO_USER bash -c 'cd ~/catkin_ws/src && git clone git@github.uio.no:robin/dyret_hardware.git'
+    sudo -H -u $SUDO_USER bash -c 'cd ~/catkin_ws/src && git clone git@github.uio.no:tonnesfn/tonnesfn_experiments.git'
+    sudo -H -u $SUDO_USER bash -c 'cd ~/catkin_ws/src && git clone git@github.uio.no:robin/dyret_simulation.git'
+
   ;;
 
   esac
